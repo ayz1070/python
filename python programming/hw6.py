22a95236567992ca151cb0edba5c923aabaff273
ayz1070 <80628496+ayz1070@users.noreply.github.com>
2023-04-13 12:21:06 +0900

    Create hw6.py

---
+++ python programming/hw6.py
@@ -0,0 +1,15 @@
+for i in range(1,10):
+    for k in range(2,6):
+        print("%d x %d = %2d" %(k,i,k*i),end ="    " )
+        if k == 5:
+            print()
+print()
+
+for i in range(1,10):
+    for k in range(6,10):
+        print("%d x %d = %2d" %(k,i,k*i),end ="    " )
+        if k==9:
+            print()
+        
+        
+    
